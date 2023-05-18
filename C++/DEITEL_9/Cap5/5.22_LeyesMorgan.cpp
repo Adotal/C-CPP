fcb44fb46c01f7333129a2d44c804c6e3fd5e48d
Adotal <adotal1484@gmail.com>
2023-05-18 07:01:27 -0600

    Updated

--- C++/DEITEL_9/Cap5/5.22_LeyesMorgan.cpp
+++ C++/DEITEL_9/Cap5/5.22_LeyesMorgan.cpp
@@ -8,20 +8,18 @@ int main(){
 	int a = 1, b = 2, c = 3, x = 10, y = 20;
 	char option = 0;
 	do{
-		cout << "\n\nSome logic expressions are equivalent,"
-				"Logically"
-				"!(a && b) is equals to (!a || !b)"
+		cout << "\n\nSome logic expressions are equivalent, logically:\n"
+				"!(a && b) is equals to (!a || !b)\n"
 				"(!a && !b) equals to !(a || b)\n"
-			 << endl
-			 << "To check it whe have: "
+				"To check it whe have:\n"
 				"a) !( x < 5 ) && !( y >= 7 )\n"
 				"b) !( a == b ) || !( c != 5 )\n"
 				"c) !(( x <= 8 ) && ( y > 4 ))\n"
-				"d) !(( a > 4) || ( c <= 6 ))\n"
+				"d) !(( a > 4 ) || ( c <= 6 ))\n"
 				"s) Exit\n"
-				"To all cases:"
+				"To all cases:\n"
 				"a = 1, b = 2, c = 3, x = 10, y = 20\n"
-				"Please select one case[a-d]: ";
+				"Please choose one case[a-d]: ";
 		cin >> option;
 
 		switch(option){
@@ -37,13 +35,27 @@ int main(){
 			break;
 
 			case 'b':
-						//"R: " << ( ? "true":"false");
+				cout << "\n!( a == b ) || !( c != 5 )\n"
+						"R: " << (!( a == b ) || !( c != 5 ) ? "true":"false")
+					 <<	"\nequals to\n"
+						"!(( a == b ) && ( c != 5 ))\n"
+						"R: " << (!(( a == b ) && ( c != 5 )) ? "true":"false");
 			break;
 
 			case 'c':
+				cout << "\n!(( x <= 8 ) && ( y > 4 ))\n"
+						"R: " << (!(( x <= 8 ) && ( y > 4 )) ? "true":"false")
+					 <<	"\nequals to\n"
+						"!( x <= 8 ) || !( y > 4 )\n"
+						"R: " << (!( x <= 8 ) || !( y > 4 ) ? "true":"false");
 			break;
 
 			case 'd':
+				cout << "\n!(( a > 4 ) || ( c <= 6 ))\n"
+						"R: " << (!(( a > 4) || ( c <= 6 )) ? "true":"false")
+					 <<	"\nequals to\n"
+						"!( a > 4 ) && !( c <= 6 )\n"
+						"R: " << (!( a > 4 ) && !( c <= 6 ) ? "true":"false");
 			break;
 
 			default:
